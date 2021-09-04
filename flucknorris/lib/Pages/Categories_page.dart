6cfe478612dfbd335a104c0f1b4e25f0b69b0670
unknown <cairo@osrreferrals.com>
2021-09-04 20:04:32 -0300

    Update

--- flucknorris/lib/Pages/Categories_page.dart
+++ flucknorris/lib/Pages/Categories_page.dart
@@ -1,3 +1,5 @@
+import 'package:flucknorris/pages/joke_page.dart';
+import 'package:flucknorris/repositories/joke_repository.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesPage extends StatefulWidget {
@@ -6,8 +8,73 @@ class CategoriesPage extends StatefulWidget {
 }
 
 class _CategoriesPageState extends State<CategoriesPage> {
+  final jokeRepository = JokeRepository();
+  Future<List<String>> futureCategories;
+
+  List<String> categories = [];
+
+  @override
+  void initState() {
+    futureCategories = jokeRepository.getCategories();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(color: Colors.red));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Escolha a categoria desejada'),
+        leading: Hero(
+          tag: 'chuck',
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/chuck.jpg'),
+          ),
+        ),
+      ),
+      // ignore: missing_return
+      body: FutureBuilder<List<String>>(
+        future: futureCategories,
+        // ignore: missing_return
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Deu erro na requisição http!"),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            final categories = snapshot.data;
+            return ListView.separated(
+                itemBuilder: (_, i) => ListTile(
+                      onTap: () async {
+                        final joke = await jokeRepository
+                            .getJokeByCategory(categories[i]);
+                        print(joke.value);
+
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (_) => JokePage(joke, categories[i])));
+                      },
+                      leading: Text('${i + 1}',
+                          style: TextStyle(
+                            fontSize: 22,
+                          )),
+                      title: Text(categories[i],
+                          style: TextStyle(
+                            fontSize: 22,
+                          )),
+                    ),
+                separatorBuilder: (_, i) => Divider(),
+                itemCount: categories.length);
+          } else {
+            return Center(
+              child: Text("Erro desconhecido!"),
+            );
+          }
+        },
+      ),
+    );
   }
 }
--- flucknorris/lib/repositories/joke_repository.dart
+++ flucknorris/lib/repositories/joke_repository.dart
@@ -1,12 +1,25 @@
 import 'dart:convert';
+import 'package:flucknorris/models/joke_model.dart';
 import 'package:http/http.dart' as http;
 
 class JokeRepository {
-  final String url = 'https://api.checknorris.io/jokes';
+  final String url = 'https://api.chucknorris.io/jokes';
 
   Future<List<String>> getCategories() async {
-    final response = await http.get(Uri.parse('$url/categories'));
-    final decoded = jsonDecode(response.body) as List;
-    return List.generate(decoded.length, (i) => decoded[i]);
+    try {
+      final response = await http.get(Uri.parse('$url/categories'));
+      final decoded = jsonDecode(response.body) as List;
+      return List.generate(decoded.length, (i) => decoded[i]);
+    } on Exception {
+      rethrow;
+    }
+  }
+
+  Future getJokeByCategory(String category) async {
+    final response =
+        await http.get(Uri.parse('$url/random?category=$category'));
+    final decoded = jsonDecode(response.body);
+    final joke = JokeModel.fromJson(decoded);
+    return joke;
   }
 }
